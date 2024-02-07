e3d4a8c042b68af73dc308c927adf413570edf45
Yannick Kremer <78233014+cl951@users.noreply.github.com>
2024-02-07 09:24:11 +0000

    Add files via upload

--- install_modflow.sh
+++ install_modflow.sh
@@ -1,11 +1,5 @@
-# Check if MODFLOW is already installed
-if ! test -f "//usr/local/bin/mf2005"; then
-	echo "Installing MODFLOW and FloPy"
-    wget --backups=1  https://github.com/cl951/MODFLOW4colab/raw/main/MODFLOW4colab.zip
-	unzip -q -o MODFLOW4colab.zip -d /content/mfbinaries/
-	chmod -R +x /content/mfbinaries
-	cp -a /content/mfbinaries/. /usr/local/bin/
-	pip install flopy
-else
-	echo "MODFLOW and FloPy already installed"
-fi
+# Install MODFLOW binaries for cl961
+wget --backups=1  https://github.com/cl951/MODFLOW4colab/raw/main/MODFLOW4colab.zip
+unzip -q -o MODFLOW4colab.zip -d /content/mfbinaries/
+chmod -R +x /content/mfbinaries
+cp -a /content/mfbinaries/. /usr/local/bin/	
